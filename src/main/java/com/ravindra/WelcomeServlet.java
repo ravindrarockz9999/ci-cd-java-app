ec601cb6549fcc742af276aa04890da0a92b3d0d
ravindrarockz9999 <ravindrarockz9999@gmail.com>
2025-10-15 22:43:00 +0530

    Change welcome message to 'Welcome to GithubActions'

--- src/main/java/com/ravindra/WelcomeServlet.java
+++ src/main/java/com/ravindra/WelcomeServlet.java
@@ -10,6 +10,6 @@ public class WelcomeServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h1>Welcome to CodeKamikaze</h1>");
+        out.println("<h1>Welcome to GithubActions</h1>");
     }
 }
